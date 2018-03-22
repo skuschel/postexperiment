acfa0f3df6c1ceaa4df66528a291481529be81dd
Alexander Blinne <alexander.blinne@uni-jena.de>
2018-03-22 15:18:25 +0100

    Enhanced field_imshow to understand rgb images

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 
 import postpic as pp
+import numpy as np
 
 def field_imshow(field, ax, force_symmetric_clim=False, **kwargs):
     #fig, ax = plt.subplots()
@@ -17,12 +18,20 @@ def field_imshow(field, ax, force_symmetric_clim=False, **kwargs):
 
     field = field.squeeze()
 
-    im = ax.imshow(field.matrix.T, extent = field.extent, **kwargs)
+    color_image = field.dimensions == 3
+    if color_image:
+        field = field/np.max(field.matrix)
+
+    im = ax.imshow(np.moveaxis(field.matrix, 0, 1), extent = field.extent[:4], **kwargs)
     ax.set_xlabel('{} [{}]'.format(field.axes[0].name, field.axes[0].unit))
     ax.set_ylabel('{} [{}]'.format(field.axes[1].name, field.axes[1].unit))
-    colorbar = ax.get_figure().colorbar(im, ax = ax)
-    colorbar.set_label('{} [{}]'.format(field.name, field.unit))
+    if not color_image:
+        colorbar = ax.get_figure().colorbar(im, ax = ax)
+        colorbar.set_label('{} [{}]'.format(field.name, field.unit))
+    else:
+        ax.set_title(field.name)
     return im
+
 
 def plot_fields_1d(fields, ax, common_name=None, plot_method='plot', plot_args=dict(), plot_kwargs=dict()):
     for i, field in enumerate(fields):
