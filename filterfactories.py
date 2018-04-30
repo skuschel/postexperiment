54150b12edf3a1f29eaadcdbc7b0db177669aaf1
Alexander Blinne <alexander.blinne@uni-jena.de>
2018-04-30 16:34:38 +0200

    Added ReadRaw filter

--- filterfactories.py
+++ filterfactories.py
@@ -208,3 +208,31 @@ def Median(field, **kwargs):
     else:
         data = scipy.ndimage.median_filter(data, size=(3, 3))
     return field.replace_data(data)
+
+@common.FilterFactory
+def ReadRaw(shot, filekey, width, height, bands=1, bands_axis=2, dtype=np.uint16, **kwargs):
+    fname = shot[filekey]
+    d = np.fromfile(fname, dtype = dtype)
+
+    shape = [height, width]
+    if bands > 1:
+        shape.insert(bands_axis, bands)
+
+    d = d.reshape(shape)
+
+    if bands > 1:
+        # switch to pixel-inverleaved mode, default for matplotlib
+        d = np.moveaxis(d, bands_index, 2)
+
+    # switch to PostPic.Field compatible axes
+    d = np.swapaxes(d, 0, 1)[:,::-1,...]
+
+    axes = []
+    axes.append(pp.Axis(name='x', unit='px', grid=np.linspace(0,width-1,width)))
+    axes.append(pp.Axis(name='y', unit='px', grid=np.linspace(0,height-1,height)))
+
+    if bands > 1:
+        axes.append(pp.Axis(name='band', unit='', grid=np.linspace(0,bands-1,bands)))
+
+
+    return pp.Field(d, name='filekey', unit='counts', axes = axes)
