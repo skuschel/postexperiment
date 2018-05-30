d00ebf4832471241c36ce78b8a7ab4fe902cd5d5
Stephan Kuschel <stephan.kuschel@gmail.com>
2018-05-30 14:32:32 -0700

    add setup.py and versioneer

--- ped/__init__.py
+++ ped/__init__.py
@@ -3,6 +3,9 @@ Copyright:
 Alexander Blinne, 2018
 '''
 
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 
 from .fitfunctions import *
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+
+from setuptools import setup, find_packages
+#from Cython.Build import cythonize
+import numpy
+import os
+
+import versioneer
+
+setup(name='ped',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      include_package_data = True,
+      author='Alexander Bilnne, Stephan Kuschel',
+      author_email='alexander.blinne@uni-jena.de',
+      description='Processing Event Data.',
+      url='https://github.com/skuschel/PED',
+      packages=find_packages(include=['ped*']),
+      include_dirs = [numpy.get_include()],
+      license='GPLv3+',
+      setup_requires=['numpy>=1.8'],
+      install_requires=['matplotlib>=1.3',
+                        'numpy>=1.8', 'numpy>=1.9;python_version<"3.0"',
+                        'scipy', 'future', 'urllib3', 'numexpr', 'functools32;python_version<"3.0"'],
+      extras_require = {
+        'h5 reader':  ['h5py']},
+      keywords = [''],
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Developers',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          'Topic :: Scientific/Engineering :: Physics',
+          'Topic :: Scientific/Engineering :: Visualization',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Operating System :: Unix',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX']
+      )
