efab71b54b8c580b91e772ea1d04c183a2948512
Stephan Kuschel <stephan.kuschel@gmail.com>
2018-06-01 00:24:51 -0700

    add h5 data source for io light data

--- ped/datasources.py
+++ ped/datasources.py
@@ -92,3 +92,93 @@ class FileSource(ShotSeriesSource):
                         shot[n] = match.group(i)
 
         return shots
+
+
+class H5ArraySource():
+    '''
+    This source describes a hdf5 data source, in which various keys contain
+    an array fo values, one for each Shot.
+
+    Stephan Kuschel, 2018
+    '''
+
+    def __init__(self, filename, validkey):
+        '''
+        args
+        ----
+        filename: str
+          the filename
+
+        validkey: str
+          key to one valid dataset
+        '''
+        self._filename = filename
+        self._validkey = validkey
+        self._validkeys = None
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def validkey(self):
+        return self._validkey
+
+    @property
+    def validkeys(self):
+        if self._validkeys is None:
+            self._validkeys = self._genkeylist(self.validkey)
+        return self._validkeys
+
+    def __len__(self):
+        import h5py
+        h5 = h5py.File(self.filename, 'r')
+        return h5[self.validkey].shape[0]
+
+    def _genkeylist(self, validkey):
+        '''
+        generate the keylist.
+
+        validkey points to an exisiting valid dataset.
+        This function will return the keys to all datasets with equal length.
+        They will be didvided into two groups: small and large.
+        "small" data contains only single values
+        "large" data contains (large) arrays.
+        For large data only a reference is saved in the dictionary.
+        '''
+        import h5py
+        length = len(self)
+        def isvaliddata(item):
+            return isinstance(item, h5py.Dataset) and item.shape[0] == length
+        retsmall = []
+        retlarge =[]
+        def visitf(key, item):
+            if not isvaliddata(item):
+                return
+            if item.shape[1:] is () or item.shape[1:] == (1,):
+                retsmall.append(key)
+            else:
+                retlarge.append(key)
+        h5 = h5py.File(self.filename, 'r')
+        h5.visititems(visitf)
+        return retsmall, retlarge
+
+    def __call__(self):
+        '''
+        returns a list of dictionaries
+        '''
+        import h5py
+        smallkeys, largekeys = self.validkeys
+        h5 = h5py.File(self.filename, 'r')  # this could be repeated
+        dsets = {key:h5[key] for key in smallkeys}  # introducing this line cuts execution time in half.
+        return [{key:dsets[key][i] for key in smallkeys} for i in range(len(self))]
+
+    def gendatadict(self, n):
+        '''
+        creates the datadict for the nth event.
+        '''
+        import h5py
+        smallkeys, largekeys = self.validkeys
+        h5 = h5py.File(self.filename, 'r')
+        ret = {key:h5[key][n] for key in smallkeys}
+        return ret
