d73435e5df96dd5d75d8c09fa3868130a6250222
Stephan Kuschel <stephan.kuschel@gmail.com>
2018-06-06 19:59:23 -0700

    add empty diagnostics submodule

---
+++ postexperiment/diagnostics/__init__.py
@@ -0,0 +1,6 @@
+'''
+"Diagnostics" are free functions mapping a `Shot` to a result.
+
+A result in this sense can be any object or number the user can use for further processing.
+However, a Diagnostics cannot reduce the data. Every shot is processed individually.
+'''
